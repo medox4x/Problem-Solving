9f757a7de313f5549c7655ea3869a86c86878819
medox4x <mohamedhaddassy1234@gmail.com>
2026-07-15 16:37:18 +0100

    Time: 6 ms (67.38%), Space: 19.2 MB (53.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = list(str(x))
+        if s == s[::-1] :
+            return True
+        else :
+            return False
+
+
+
+        
+        
