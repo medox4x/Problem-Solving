15d85a0b84ba4d9a44adc138ad2f48aadc86e586
medox4x <mohamedhaddassy1234@gmail.com>
2026-09-16 17:47:48 +0100

    Time: 495 ms (14.83%), Space: 0B (100%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select actor_id,director_id
+with result as (select *,count(timestamp)
 from ActorDirector
 group by director_id, actor_id
-having count(timestamp) >=3;
+having count(timestamp) >=3)
+select actor_id,director_id 
+from result;
+
