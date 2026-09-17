2290399c22016b9d68b36b9f4422f921000dfdf9
medox4x <mohamedhaddassy1234@gmail.com>
2026-09-17 13:35:13 +0100

    Time: 2899 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select product_name,year,price
 from Sales,Product
-where Sales.product_id = Product.product_id
+where Sales.product_id = Product.product_id;
