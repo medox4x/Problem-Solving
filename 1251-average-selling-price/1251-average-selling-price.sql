931639ddc41292ed692b1202f0d2bd86326d92a3
medox4x <mohamedhaddassy1234@gmail.com>
2026-09-24 16:19:40 +0100

    Time: 969 ms (38.92%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -3,3 +3,4 @@ select P.product_id ,coalesce(round(sum(U.units*P.price)/sum(U.units),2),0) as a
 from Prices as P left join UnitsSold U
 on P.product_id = U.product_id and U.purchase_date between P.start_date and P.end_date
 group by P.product_id
+# here I learned to use coalesce() func
