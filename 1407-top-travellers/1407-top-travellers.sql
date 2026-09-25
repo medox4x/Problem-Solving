4ed115f9f15bca0a1ac377662bafe818ffd0932a
medox4x <mohamedhaddassy1234@gmail.com>
2026-09-25 11:48:07 +0100

    Time: 912 ms (63.67%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -3,4 +3,4 @@ select U.name,coalesce(sum(R.distance),0) as travelled_distance
 from Users as U left join Rides as R
 on U.id = R.user_id
 group by R.user_id
-order by sum(R.distance) desc, U.name asc
+order by sum(R.distance) desc, U.name asc;
