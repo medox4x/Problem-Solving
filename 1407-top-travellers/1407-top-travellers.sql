925751afc67fb83124ac5918dc7b52382d955c6a
medox4x <mohamedhaddassy1234@gmail.com>
2026-09-25 11:47:58 +0100

    Time: 1710 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select U.name,coalesce(sum(R.distance),0) as travelled_distance
+from Users as U left join Rides as R
+on U.id = R.user_id
+group by R.user_id
+order by sum(R.distance) desc, U.name asc
